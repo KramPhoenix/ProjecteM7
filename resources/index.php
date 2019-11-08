330a15dc29fb71d736d61233851bef1c65cc5bc0
KramPhoenix <marcdmn1999@gmail.com>
2019-11-08 19:33:17 +0100

    Front y config de la base de datos

--- resources/index.php
+++ resources/index.php
@@ -1,9 +1,22 @@
+
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css">
 </head>
 <body>
-	<h1>IMMOBILIARY</h1>
+<div class="d-flex justify-content-center">
+	<h1>IMMOBILIARY LOGIN</h1>
+</div>
+<div class="d-flex justify-content-center">
+	<form method="POST" action="<?= $_SERVER[PHP_SELF];?>">
+		<input type="text" name="user" placeholder="User">
+		<input type="password" name="pwd" placeholder="Password">
+		<input type="submit" name="submit" value="Entra">
+	</form>
+</div>
 </body>
 </html>
